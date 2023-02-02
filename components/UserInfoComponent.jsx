97b135ba60a42912c3eb3e6f23b0132d033b9e28
AmitMali <amit23289@gmail.com>
2023-02-02 11:39:02 +0530

    using users data to create users card passing props to component example

--- components/UserInfoComponent.jsx
+++ components/UserInfoComponent.jsx
@@ -1,19 +1,26 @@
-function UserInfoComponent() {
+function UserInfoComponent(props) {
+  const { name, email, username, phone, website, company, address } =
+    props.data;
   return (
     <>
-      <div className="card" style={{ width: "18rem" }}>
+      <div className="card">
         <div className="card-body">
-          <h5 className="card-title">Card title</h5>
-          <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
+          <h5 className="card-title">{name}</h5>
+          <h6 className="card-subtitle mb-2 text-muted">{username}</h6>
           <p className="card-text">
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
+            <ul className="list-group list-group-flush">
+              <b>Address:</b>
+              <li className="list-group-item">Street:{address.street}</li>
+              <li className="list-group-item">Street:{address.suit}</li>
+              <li className="list-group-item">Street:{address.city}</li>
+              <li className="list-group-item">Street:{address.zipcode}</li>
+            </ul>
           </p>
           <a href="#" className="card-link">
-            Card link
+            {email}
           </a>
           <a href="#" className="card-link">
-            Another link
+            {phone}
           </a>
         </div>
       </div>
--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import ClassComponentExample from "../components/ClassComponentExample";
 import ProductCart from "../components/ProductCart";
+import UserInfoComponent from "../components/UserInfoComponent";
+import { users } from "../utils/loadUsers";
 export default function App() {
+  console.log(users);
   return (
-    <div>
-      <ProductCart name="Macbook Pro" price="140" />
-      <ProductCart name="Macbook Air" price="100" />
-      <ProductCart name="Macbook Ultra" price="160" />
-
-      <ClassComponentExample msg="hello from class component" />
+    <div className="container">
+      <div className="row">
+        {users.map((user) => {
+          return (
+            <div className="col-lg-4">
+              <UserInfoComponent data={user} />
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
